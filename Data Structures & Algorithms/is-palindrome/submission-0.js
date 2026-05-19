1a620a3f4523e6e466cdf90965c362bae95c3305
Om Dhawale <219327955+om-dhawale@users.noreply.github.com>
2026-05-19 17:15:46 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    alphaNum(c) {
+        return (
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9')
+        );
+    }
+
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+        while(l<r){
+            while (l < r && !this.alphaNum(s[l])) {
+                l++;
+            }
+            while (r > l && !this.alphaNum(s[r])) {
+                r--;
+            }
+            if(s[l].toLowerCase() !== s[r].toLowerCase()){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
